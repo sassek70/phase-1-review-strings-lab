8e9fc2cf04c5582d8b97225113119a9fea3823f2
Kevin Sasse <sassek70@gmail.com>
2022-09-16 19:47:51 -0400

    finished lab

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code in this file!
+const currentUser = 'Kevin Sasse';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`; 
+
+const shortGreeting = `Welcome, ${currentUser[0]}!`;
